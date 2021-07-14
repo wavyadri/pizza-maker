f1ea981207ec8759a96454482d9217041f2c2070
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-14 14:35:57 -0400

    Create Home

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <section id='home'>
+            <div className="home-text">
+                <h1>Welcome to Pizza Maker</h1>
+                <div className="home-buttons">
+                    <button>Our Menu</button>
+                    <button>Make Your Own</button>
+                </div>
+            </div>            
+        </section>
+    )
+}
+
+export default Home
