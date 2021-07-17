5b33d35b70a426a758e2b23c3e702a139d47f1e1
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-17 16:56:45 -0400

    Update styles
    Restructing navbar styles for web and responsive formats.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import {HiOutlineShoppingCart} from 'react-icons/hi'
+import {FaBars} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <nav id='navbar'>
             <div className="navbar-items">
-                <Link className="link" to='/menu'><div className="logo">pizza maker</div></Link>
-                <ul className='navbar-list'>
-                    <Link className="link" to='/menu'><li><p>our menu</p></li></Link>
-                    <Link className="link" to='/make-your-own'><li><p>make your own</p></li></Link>
-                </ul>
-                <Link className='link' to='/shopping-cart'><HiOutlineShoppingCart className='cart-icon'/></Link>
+                <div className="logo">
+                    <Link className="link" to='/menu'><p>pizza maker</p></Link>
+                    <button className='navbar-toggle'><FaBars/></button>
+                </div>
+                <div className="navbar-links">
+                    <ul className='navbar-list'>
+                        <Link className="link" to='/menu'><li><p>our menu</p></li></Link>
+                        <Link className="link" to='/make-your-own'><li><p>make your own</p></li></Link>
+                    </ul>
+                    <Link className='link shopping-cart' to='/shopping-cart'><HiOutlineShoppingCart className='cart-icon'/></Link>
+                </div>
             </div>
         </nav>
     )
