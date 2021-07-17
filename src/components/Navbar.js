7c213e01c5a5dd2a36c139af090ad5aea60cc8e8
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-16 23:02:50 -0400

    Complete Nav
    Created navbar for all pages. Styled for web.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import {HiOutlineShoppingCart} from 'react-icons/hi'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div>
-           Navber 
-        </div>
+        <nav id='navbar'>
+            <div className="navbar-items">
+                <Link className="link" to='/menu'><div className="logo">pizza maker</div></Link>
+                <ul className='navbar-list'>
+                    <Link className="link" to='/menu'><li><p>our menu</p></li></Link>
+                    <Link className="link" to='/make-your-own'><li><p>make your own</p></li></Link>
+                </ul>
+                <Link className='link' to='/shopping-cart'><HiOutlineShoppingCart className='cart-icon'/></Link>
+            </div>
+        </nav>
     )
 }
 
