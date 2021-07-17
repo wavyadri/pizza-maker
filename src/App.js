e97cc5866cd845f3e37fd0fc58059758c6b2bcc0
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-16 20:34:47 -0400

    Update App
    Change Router to only show Navbar on all pages that are not Home.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // pages
+import Navbar from './components/Navbar'
 import Home from './components/Home'
 import FullMenu from './components/FullMenu'
 import Custom from './components/Custom'
 
 function App() {
+  // show navbar and footer on all pages other than Home
+  const DefaultRoutes = () => {
+    return (
+      <>
+        <Navbar/>
+        <Switch>
+          <Route path='/menu'>
+            <FullMenu/>
+          </Route>
+          <Route path='/make-your-own'>
+            <Custom/>
+          </Route>
+        </Switch>
+      </>
+    )
+  }
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
           <Home/>
         </Route>
-        <Route path='/menu'>
-          <FullMenu/>
-        </Route>
-        <Route path='/make-your-own'>
-          <Custom/>
-        </Route>
+        <Route component={DefaultRoutes}/>
       </Switch>
     </Router>
   );
