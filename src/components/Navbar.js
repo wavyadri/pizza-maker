22c40f79be49ccdb17b546cfec258d50248a3dd6
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-18 22:45:24 -0400

    Complete Navbar.js
    Web, tablet and mobile versions of navbar completed. UseRef used for mobile version to adjust dropdown menu height if more links need to be added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,22 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     const [showLinks, setShowLinks] = useState(false);
+    // ref for div navbar-links
+    const navbarLinksRef = useRef(null); 
+    // ref for ul navbar-list
+    const navbarListRef = useRef(null);
+
+    // run on every change in showLinks (aka every click of button)
+    useEffect(() => {
+        // check height of navbar-list
+        const linksHeight = navbarListRef.current.getBoundingClientRect().height;
+        // use links height to set height of navbar-links
+        if(showLinks) {
+            navbarLinksRef.current.style.height = `${linksHeight}px`
+        } else {
+            navbarLinksRef.current.style.height = '0px'
+        }
+    }, [showLinks])
     
     return (
         <nav id='navbar'>
@@ -13,8 +29,8 @@ const Navbar = () => {
                     <Link className="link logo-text" to='/menu'><p>pizza maker</p></Link>
                     <button className='navbar-toggle' onClick={() => setShowLinks(!showLinks)}><FaBars/></button>
                 </div>
-                <div className={`${showLinks ? 'navbar-links show-links' : 'navbar-links'}`}>
-                    <ul className='navbar-list'>
+                <div className='navbar-links' ref={navbarLinksRef}>
+                    <ul className='navbar-list' ref={navbarListRef}>
                         <li><Link className="link" to='/menu'><p>our menu</p></Link></li>
                         <li><Link className="link" to='/make-your-own'><p>make your own</p></Link></li>
                         <li><Link className='link shopping-cart' to='/shopping-cart'><HiOutlineShoppingCart className='cart-icon'/></Link></li>
