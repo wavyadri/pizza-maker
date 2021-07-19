f8b9173e3c260609b2f6736a91ba68a9cb8eb8cb
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-18 23:14:34 -0400

    Create Header.js
    Header to be used on FullMenu and Custom.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// h1 match route
+// remove any '-' and replace with ' '
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Page Header</h1>
+        </div>
+    )
+}
+
+export default Header
