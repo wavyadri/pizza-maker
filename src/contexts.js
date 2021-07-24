8015c9898da6b3e3360fd76a2c66581409cc3e19
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-24 13:51:36 -0400

    Update contexts.js
    Include menu.js in context.

--- src/contexts.js
+++ src/contexts.js
@@ -1,13 +1,23 @@
 import React, {useState, useContext, useEffect} from 'react'
+import menu from './components/menu'
 
 const AppContext = React.createContext();
 
 // always access children
 const AppProvider = ({children}) => {
 
-    // const [catgeories, setCategories] = useState(allCategories);
+    // M E N U
+    const [menuItems, setMenuItems] = useState(menu);
+    // new array of only unique categories
+    const allCatgeories = [...new Set(menu.map((item) => item.category))];
+    const [categories, setCategories] = useState(allCatgeories)
 
-    return <AppContext.Provider value={'hello world'}>
+    const filterMenu = (category) => {
+        const newMenu = menu.filter((item) => item.category === category);
+        setMenuItems(newMenu);
+    }
+
+    return <AppContext.Provider value={{menuItems}}>
         {children}
     </AppContext.Provider>
 }
@@ -17,4 +27,5 @@ export const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
+// we want to export the context and the provider
 export {AppContext, AppProvider}
