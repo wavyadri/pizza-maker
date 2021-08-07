cdb61b1ab8aecb46d2c6b3a6dd4c719266dfd37a
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-08-07 15:25:23 -0400

    Create Modal.js
    To pop up upon clicking confirm order button in Checkout.js

---
+++ src/components/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Modal = () => {
+    return (
+        <div id='modal'>
+            <div className="container">
+                <h1>thank you!</h1>
+                <p>your order has been placed.</p>
+                <button>
+                    new order
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
