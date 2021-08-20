d88b6a246b0576701a96a6169388fa6d6392ee1d
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-08-20 17:41:50 -0400

    Create Error.js
    Error page for all other routes. Will have a button redirecting to FullMenu.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {BiArrowBack} from 'react-icons/bi'
 
 const Error = () => {
     return (
-        <div>
-            Error
-        </div>
+        <section id='error'>
+            <h1>Woops! You went the wrong way.</h1>
+            <Link to='/our-menu' className='btn'>
+                <BiArrowBack className='icon'/>
+                <p>our menu</p>
+            </Link>
+        </section>
     )
 }
 
